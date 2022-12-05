e1c48d3d29e683506ac7c8f76ab42eb2c6f8833d
Jaro233 <jaroslaw.waliczek@vp.pl>
2022-12-05 16:27:26 +0100

    rework

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const hamburgerBtn = document.querySelector(".hamburger_btn");
-const dropDownMenu = document.querySelector(".dropdown__menu");
-const h1_text_and_arrow = document.querySelector(".h1_text_and_arrow");
+const dropDownMenu = document.querySelector(".dropdown-menu");
+const h1_text_and_arrow = document.querySelector(".header-content");
 
 function open_closeMenu() {
     console.log("toggle")
     dropDownMenu.classList.toggle("hidden")
-    h1_text_and_arrow.classList.toggle("display_col")
+    // h1_text_and_arrow.classList.toggle("display_col")
     h1_text_and_arrow.classList.toggle("hidden")
-    dropDownMenu.classList.toggle("display_col")
-
+    // dropDownMenu.classList.toggle("display_col")
 }
 
 
